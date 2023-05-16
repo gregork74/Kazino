c4b19bb5d51ab67fe73e0b39b1e8999190b43b2e
gregork74 <114024753+gregork74@users.noreply.github.com>
2023-05-16 08:33:14 +0200

    Update index.php
    favicon

--- casino/index.php
+++ casino/index.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="icon" type="image/x-icon" href="photos/favicon3.png">
+	<link rel="icon" href="photos/favicon3.png" >
 	<link rel="stylesheet" href="css/stylesheet.css">
 	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
